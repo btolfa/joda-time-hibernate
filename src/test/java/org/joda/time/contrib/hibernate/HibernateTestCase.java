ab9aa6a74234e04bd09f73d24383e1ac714263ab
Tengiz Sharafiev <btolfa@gmail.com>
2012-09-17 15:51:09 +0400

    rewrite getSessionFactory by http://stackoverflow.com/q/7986750/399601

--- src/test/java/org/joda/time/contrib/hibernate/HibernateTestCase.java
+++ src/test/java/org/joda/time/contrib/hibernate/HibernateTestCase.java
@@ -16,34 +16,38 @@
 package org.joda.time.contrib.hibernate;
 
 import junit.framework.TestCase;
-
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.dialect.HSQLDialect;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 import org.hibernate.tool.hbm2ddl.SchemaUpdate;
 
 import java.sql.Connection;
 import java.sql.Statement;
 
-public abstract class HibernateTestCase extends TestCase
-{
+public abstract class HibernateTestCase extends TestCase {
     private SessionFactory factory;
     private Configuration cfg;
+    private ServiceRegistry serviceRegistry;
 
-    protected SessionFactory getSessionFactory()
-    {
-        if (this.factory == null)
-        {
-                    cfg = new Configuration();
+    protected SessionFactory getSessionFactory() {
+        if (this.factory == null) {
+            cfg = new Configuration();
 
-                    setupConfiguration(cfg);
+            setupConfiguration(cfg);
 
             cfg.setProperty("hibernate.connection.driver_class", "org.hsqldb.jdbcDriver");
             cfg.setProperty("hibernate.connection.url", "jdbc:hsqldb:mem:hbmtest" + getClass().getName());
             cfg.setProperty("hibernate.dialect", HSQLDialect.class.getName());
 
             cfg.setProperty("hibernate.show_sql", "true");
-            SessionFactory factory = cfg.buildSessionFactory();
+
+            serviceRegistry = new ServiceRegistryBuilder()
+                    .applySettings(cfg.getProperties())
+                    .buildServiceRegistry();
+
+            SessionFactory factory = cfg.buildSessionFactory(serviceRegistry);
 
             SchemaUpdate update = new SchemaUpdate(cfg);
             update.execute(true, true);
@@ -53,22 +57,20 @@ public abstract class HibernateTestCase extends TestCase
         return factory;
     }
 
-    protected void tearDown() throws Exception
-    {
-            final String[] dropSQLs = cfg.generateDropSchemaScript(new HSQLDialect());
-            final Connection connection = getSessionFactory().openSession().connection();
-            try {
-                Statement stmt = connection.createStatement();
-                for (int i = 0; i < dropSQLs.length; i++) {
-                    //System.out.println("dropSQLs[i] = " + dropSQLs[i]);
-                    stmt.executeUpdate(dropSQLs[i]);
-                }
-            } finally {
-                connection.close();
+    protected void tearDown() throws Exception {
+        final String[] dropSQLs = cfg.generateDropSchemaScript(new HSQLDialect());
+        final Connection connection = getSessionFactory().openSession().connection();
+        try {
+            Statement stmt = connection.createStatement();
+            for (int i = 0; i < dropSQLs.length; i++) {
+                //System.out.println("dropSQLs[i] = " + dropSQLs[i]);
+                stmt.executeUpdate(dropSQLs[i]);
             }
+        } finally {
+            connection.close();
+        }
 
-            if (this.factory != null)
-        {
+        if (this.factory != null) {
             this.factory.close();
             this.factory = null;
         }
